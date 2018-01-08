865b1a125ed5f4cc3bd103c738c24fa3826e536a
Eugenia Schilling <eugenia.schilling@yahoo.com>
2018-01-09 00:31:17 +0100

    Fehler korrigiert

--- src/test/java/EclipseMavenTest/EclipseMavenTest/SkypeTest.java
+++ src/test/java/EclipseMavenTest/EclipseMavenTest/SkypeTest.java
@@ -40,7 +40,7 @@ public class SkypeTest {
 				.click();
 		driver.findElement(By.id("signIn")).click();
 		driver.findElement(By.id("idSIButton9")).click();
-		driver.findElement(By.id("usernameError222")).click();
+		driver.findElement(By.id("usernameError")).click();
 		try {
 			assertEquals(
 					driver.findElement(By.id("usernameError")).getText(),
